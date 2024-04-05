1febaad746a85c7150ff970c02f6f8e581fcd6ff
Minhyeong Kim <ytzka14@snu.ac.kr>
2024-04-05 15:35:46 +0900

    relative import fixed

--- llm_example/main.py
+++ llm_example/main.py
@@ -1,10 +1,18 @@
 import asyncio
 
-from llm.ai_handler import AiHandler
-from llm.utils import run_prompt
-from settings.config_loader import get_settings
-
 if __name__ == "__main__":
+    if __package__ is None:
+      import sys
+      from os import path
+      sys.path.append(path.dirname( path.dirname( path.abspath(__file__) ) ))
+      from llm.ai_handler import AiHandler
+      from llm.utils import run_prompt
+      from settings.config_loader import get_settings
+    else:
+      from ..llm.ai_handler import AiHandler
+      from ..llm.utils import run_prompt
+      from ..settings.config_loader import get_settings
+    
     handler = AiHandler()
 
     prompt = get_settings()["ask_capital_city_prompt"]
