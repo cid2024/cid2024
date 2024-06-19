5413f707c033978c46d3ad984b3d142afa302242
Gyojun Youn <youngyojun@gmail.com>
2024-06-20 03:09:47 +0900

    chore: Change db

--- settings/db_loader.py
+++ settings/db_loader.py
@@ -13,7 +13,7 @@ global_db = mysql.connector.connect(
     port = 3306,
     user = settings.mysql.user,
     password = settings.mysql.password,
-    database = "chery_society"
+    database = "chery_history"
 )
 cursor = global_db.cursor()
 
