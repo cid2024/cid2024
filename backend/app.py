8817529d36ddf1c7a31772d1927645227b77213d
Gyojun Youn <youngyojun@gmail.com>
2024-06-20 01:00:42 +0900

    feat: Set CORS for backend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
+
 from bank import models
 from bank.loader import get_problems_dict
 
 app = Flask(__name__)
+CORS(app)
 
 problems_dict: dict[str, models.Problem] = {}
+
 
 @app.route('/')
 def home():
     return jsonify(message="Welcome to the Flask backend!")
 
+
 @app.route('/problems/<string:id>', methods=['GET'])
-def get_data(id):
+def get_data(id: str) -> Response:
     return jsonify(problems_dict[id])
+
 
 if __name__ == '__main__':
     problems_dict = get_problems_dict()
